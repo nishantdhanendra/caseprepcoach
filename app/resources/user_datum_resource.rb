94ff8083ef712dd8dff32998eb31c3bab3df5801
anonymous anonymous <nishant.dhanendra@kellogg.northwestern.edu>
2021-12-10 02:19:20 +0000

    Generate graphiti resources for user_data

---
+++ app/resources/user_datum_resource.rb
@@ -0,0 +1,16 @@
+class UserDatumResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :user_name, :string
+  attribute :user_goal, :string
+  attribute :self_assessment_score, :integer
+  attribute :email, :string
+  attribute :password, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
